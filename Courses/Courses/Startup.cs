aed3775195412886acabce59cd183c16a62bbda2
Umais Siddiqui <umais20@yahoo.com>
2017-10-08 15:34:32 -0400

    first commit

---
+++ Courses/Courses/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Courses.Startup))]
+namespace Courses
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
